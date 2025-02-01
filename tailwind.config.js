ee2022497f686708128ec138f691dc799728165d
Jibon Krishna <jibonkrishnaroysetu@gmail.com>
2025-02-01 11:00:40 +0600

    completed header section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+            'green1': '#179800',
+            'green2': '#115414',
+            'blue1': '#102861',
+            'blue2': '#2A4D97',
+            'bg1': '#EFEBE3',
+            'text1': '#111111',
+            'text2': '#FFFFFF',
+        }
+      },
     },
     plugins: [],
   }
